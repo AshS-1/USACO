f18ed8c298beafe428d53eda1e7bda8aa4411cf7
AshS-1 <108997371+AshS-1@users.noreply.github.com>
2023-08-10 10:03:49 -0700

    Update hay.java

--- Bronze/2023/hay.java
+++ Bronze/2023/hay.java
@@ -1,1 +1,45 @@
+// 2023 Bronze #1 February Contest
 
+import java.util.*;
+public class hay {
+	public static void main(String[] args){
+		
+		Scanner first = new Scanner(System.in);
+		long loop = first.nextLong();
+		long days = first.nextLong();
+		
+		long when = first.nextLong();
+		long hay = first.nextLong();
+
+		long whennow = 0;
+		long haynow = 0;
+
+		long total = 0;
+
+		for (long i = 0; i < loop; i++) {
+			if (first.hasNext()){
+				whennow = first.nextLong();
+				haynow = first.nextLong();
+			} else {
+				whennow = days + 1;
+				haynow = hay;
+			} 
+			
+
+			long whenchange = whennow - when;
+
+			if (whenchange > hay) {
+				total = total + hay;
+				hay = haynow;
+			} else {
+				total = total + whenchange;
+				hay = haynow + hay - whenchange;
+			}
+
+			when = whennow;
+			
+		}
+
+		System.out.print(total);
+	}
+}
